dfd33cbf3820f4411692e0139d5d2137380133d7
NithishP1398 <139207304+NithishP1398@users.noreply.github.com>
2023-10-04 16:49:04 +0530

    Update nexusDownload.groovy

--- vars/nexusDownload.groovy
+++ vars/nexusDownload.groovy
@@ -5,7 +5,7 @@ def call(version,outputFile,repoName,groupId,artifactId, extension){
   withCredentials([usernameColonPassword(credentialsId: '5a7f8600-4b51-42ce-ab7a-4112ae9e8a73',variable:'USERPASS')]) {
     sh """
     curl -u $USERPASS -o ${outputFile} -X GET "${NEXUS_URL}/repository/${repoName}/${groupId}/${artifactId}/${version}/mvn-demo-${version}.${extension}"
-  """
+  """ }
 }
 
 //  curl -u admin:admin -o ${outputfile} -X GET "${NEXUS_URUL}/repository/doctor-online-release/in/javahome/doctor-online/${version}/doctor-online-${version}.war"
